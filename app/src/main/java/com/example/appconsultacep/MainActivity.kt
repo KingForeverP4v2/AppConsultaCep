0d383b15da5b22a022e57968ad3d0df32412852e
Void <nickkte606@gmail.com>
2026-09-18 09:11:33 -0300

    dasdas

--- app/src/main/java/com/example/appconsultacep/MainActivity.kt
+++ app/src/main/java/com/example/appconsultacep/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import com.example.appconsultacep.Api.ViaCepClient
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +25,26 @@ class MainActivity : AppCompatActivity() {
         }
         val edtCep = findViewById<EditText>(R.id.edtCep)
         val btnConsultar = findViewById<Button>(R.id.btnConsultar)
+        val txtLogradouro = findViewById<EditText>(R.id.txtLogradouro)
+        val txtBairro = findViewById<EditText>(R.id.txtBairro)
+        val txtUf = findViewById<EditText>(R.id.txtUF)
+        val txtLocal = findViewById<EditText>(R.id.txtCidade)
+        val txtDDD = findViewById<EditText>(R.id.txtDDD)
         btnConsultar.setOnClickListener {
-            val cep = edtCep.text.toString()
-             if (cep.length != 8) {
-                 Toast.makeText(this,"CEP inválido", Toast.LENGTH_LONG)
-             }
+           val cep = edtCep.text.toString()
+            if (cep.length != 8){
+                edtCep.error = "CEP inválido"
+                edtCep.requestFocus()
+                return@setOnClickListener
+            }
+            lifecycleScope.launch {
+                val endereco = ViaCepClient.instance.buscarEndereco(cep)
+                txtLogradouro.setText(endereco.logradouro)
+                txtBairro.setText(endereco.bairro)
+                txtUf.setText(endereco.uf)
+                txtLocal.setText(endereco.localidade)
+                txtDDD.setText(endereco.ddd)
+            }
         }
     }
 }
